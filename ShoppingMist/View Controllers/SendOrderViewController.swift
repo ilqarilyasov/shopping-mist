af305e7a0e2b19ee39f6b92e5047aff243cccd51
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2019-01-19 18:47:59 -0600

    Implement SendOrderViewController's updateViews function

--- ShoppingMist/View Controllers/SendOrderViewController.swift
+++ ShoppingMist/View Controllers/SendOrderViewController.swift
@@ -18,18 +18,38 @@ class SendOrderViewController: UIViewController {
     
     // MARK: - Properties
     
-    var shoppingItemController: ShoppingItemController?
+    var shoppingItemController: ShoppingItemController? {
+        didSet { updateViews() }
+    }
     
     // MARK: - Lifecycle functions
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        updateViews()
     }
     
     // MARK: - Actions
     
     @IBAction func sendOrderButtonTapped(_ sender: Any) {
         
+    }
+    
+    // MARK: - Update Views
+    
+    private func updateViews() {
+        guard let shoppingItemController = shoppingItemController,
+            isViewLoaded else { return }
+        
+        let addedItems = shoppingItemController.addedItems
+        let count = addedItems.count
+        
+        if addedItems.isEmpty {
+            howManyItemsLabel.text = "You currently don't have any item in your shopping list"
+        } else if addedItems.count == 1 {
+            howManyItemsLabel.text = "You currently have 1 item in your shopping list"
+        } else {
+            howManyItemsLabel.text = "You currently have \(count) items in your shopping list"
+        }
     }
 }
